5c434e09ad2ef8d13135809d7cc85e35eb108f48
CpC <jasperli0407@gamil.com>
2020-11-30 16:42:39 +0800

    add 使用者註冊表單

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm , RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,9 +19,22 @@ def login():
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
-@auth.route('logout')
+@auth.route('/logout')
 @login_required
 def logout():
     login_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can login now.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
