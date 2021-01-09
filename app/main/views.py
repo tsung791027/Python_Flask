08a29e54db383e2d4e3456b33291810827e036b2
MyMac <tsung791027@gmail.com>
2021-01-09 23:51:49 +0800

    add profile form and views , html

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,13 @@
 #主要的視窗
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
+from flask_login import login_required, current_user
+
+
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -33,3 +36,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been upgrade.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
